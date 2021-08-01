72b60536719f2a04a6bb382c37f98c654e043c8d
codwerk <louisws@outlook.fr>
2021-08-01 21:57:19 +0200

    update tableau object

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,6 +1,49 @@
+const products = [
+  {
+    id:1,
+    titre:'Nike Air Jordan 1',
+    description: 'Nike Air Jordan Footwear basketball sneakers.',
+    price: 99,
+    img: './assets/img/img3.png'
+  },
+  {
+    id:2,
+    titre:'Nike Air Jordan 2',
+    description: 'Nike Air Jordan Footwear basketball sneakers.',
+    price: 40,
+    img:'./assets/img/img2.png'
+  },
+  {
+    id:3,
+    titre:'Nike Air Jordan 2',
+    description: 'Nike Air Jordan Footwear basketball sneakers.',
+    price: 60,
+    img: './assets/img/img1.png'
+  },
+  {
+    id:4,
+    titre:'Nike Air Jordan 4',
+    description: 'Nike Air Jordan Footwear basketball sneakers.',
+    price: 60,
+    img: './assets/img/img4.png'
+  },
+  {
+    id:5,
+    titre:'Nike Air Jordan 5',
+    description: 'Nike Air Jordan Footwear basketball sneakers.',
+    price: 60,
+    img: './assets/img/img6.png'
+  }   
+  ]
+
 const Home = {
   template: '#home',
-  name: 'Home'
+  name: 'Home',
+  data:() => {
+    return {
+      products
+    }
+  }
 }
 
 const UserSettings = {
